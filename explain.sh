915e75699172982810a949ed15fda38206f7db6c
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-08 15:20:09 +0300

    Explained

--- explain.sh
+++ explain.sh
@@ -1,6 +1,7 @@
-echo "Annabel Church"
-echo "9437737"
-echo "Blue Honda"
-echo "Joe Germuska"
-echo "Hellen Maher"
-echo "Erika Owens"
+printf "Annabel Church\n"
+printf "699607\n"
+printf "Blue Honda\n"
+printf "Joe Germuska\n"
+printf "Hellen Maher\n"
+printf "Erika Owens\n"
+
