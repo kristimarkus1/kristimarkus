9145147ce82202f81911ddb08cb6cce7d6294b7b
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-08 17:00:39 +0300

    Hardddd

---
+++ teacher.sh
@@ -0,0 +1,4 @@
+export INTERVIEW_NAME="`find ./ -type f -name 'Buckingham_Place' -exec cat {} \; | grep 'INTERVIEW' | sed 's/.*#//' `"
+echo $INTERVIEW_NAME
+find ./ -type f -name "interview-$INTERVIEW_NAME" -exec cat {} \;
+echo $MAIN_SUSPECT
