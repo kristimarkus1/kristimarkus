e64b4d776ab328b00ffb45ff13095c2ef6e3c821
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-09 23:37:01 +0300

    Comb

---
+++ printcombn.go
@@ -0,0 +1,39 @@
+package piscine
+
+import (
+	"fmt"
+)
+
+// PrintCombN prints all possible combinations of n different digits in ascending order.
+func PrintCombN(n int) {
+	if n <= 0 || n >= 10 {
+		return // n should be within the range (0, 10)
+	}
+
+	nums := make([]int, n)
+	printCombination(nums, n, 0, 0)
+}
+
+// Helper function to recursively generate and print combinations
+func printCombination(nums []int, n, index, start int) {
+	if index == n {
+		printNumber(nums)
+		return
+	}
+
+	for i := start; i <= 9; i++ {
+		nums[index] = i
+		printCombination(nums, n, index+1, i+1)
+	}
+}
+
+// Helper function to print a combination as a number
+func printNumber(nums []int) {
+	for i, num := range nums {
+		if i > 0 {
+			fmt.Print(", ")
+		}
+		fmt.Print(num)
+	}
+	fmt.Println()
+}
