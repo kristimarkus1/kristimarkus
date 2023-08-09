538d70921747a76ec4c22ac3670c2e6224fed686
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-09 23:47:21 +0300

    Nbr

--- printnbr.go
+++ printnbr.go
@@ -1,38 +1,29 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
-func PrintNbr(n int) {
-	if n == -9223372036854775808 {
-		printString("-9223372036854775808")
-		return
-	}
-
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-
-	printPositiveNbr(n)
-}
-
-func printPositiveNbr(n int) {
-	if n == 0 {
+func PrintNum(num int) {
+	i := '0'
+	if num == 0 {
 		z01.PrintRune('0')
 		return
 	}
-
-	if n > 9 {
-		printPositiveNbr(n / 10)
+	for j := 1; j <= num%10; j++ {
+		i++
 	}
-
-	z01.PrintRune(rune(n%10 + '0'))
+	for j := -1; j >= num%10; j-- {
+		i++
+	}
+	if num/10 != 0 {
+		PrintNum(num / 10)
+	}
+	z01.PrintRune(i)
+	return
 }
 
-func printString(s string) {
-	for _, ch := range s {
-		z01.PrintRune(ch)
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
 	}
+	PrintNum(n)
 }
