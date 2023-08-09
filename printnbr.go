cc6eed98898cb8e2aa72f8170fe0eb704c60c1e6
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-09 23:02:54 +0300

    Nbr

--- printnbr.go
+++ printnbr.go
@@ -4,15 +4,23 @@ import (
 	"github.com/01-edu/z01"
 )
 
+func printPositiveNbr(n int) {
+	if n > 9 {
+		printPositiveNbr(n / 10)
+	}
+	z01.PrintRune(rune(n%10 + '0'))
+}
+
 func PrintNbr(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+
 	if n < 0 {
 		z01.PrintRune('-')
 		n = -n
 	}
 
-	if n > 9 {
-		PrintNbr(n / 10)
-	}
-
-	z01.PrintRune(rune(n%10 + '0'))
+	printPositiveNbr(n)
 }
