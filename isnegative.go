2b20434a16d295492d52a2ae90c4266b8e32cf6e
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-09 14:46:10 +0300

    Is it?

--- isnegative.go
+++ isnegative.go
@@ -4,9 +4,9 @@ import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	if nb < 0 {
-		z01.PrintRune('F')
-	} else {
 		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
 	}
 	z01.PrintRune('\n')
 }
