23ded410abeddc744091bc2b8ed2ff4929fd41da
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-09 22:50:30 +0300

    Nbr

--- printnbr.go
+++ printnbr.go
@@ -1,37 +1,8 @@
 package piscine
 
-import (
-	"math/big"
+import "github.com/01-edu/z01"
 
-	"github.com/01-edu/z01"
-)
-
-func PrintNbr(n int64) {
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
-
-	bigIntN := new(big.Int).SetInt64(n)
-	if n < 0 {
-		z01.PrintRune('-')
-		bigIntN.Abs(bigIntN)
-	}
-
-	ten := big.NewInt(10)
-	zero := big.NewInt(0)
-	reverse := new(big.Int)
-
-	for bigIntN.Cmp(zero) > 0 {
-		remainder := new(big.Int)
-		bigIntN, remainder = bigIntN.DivMod(bigIntN, ten, remainder)
-		reverse = reverse.Mul(reverse, ten)
-		reverse = reverse.Add(reverse, remainder)
-	}
-
-	for reverse.Cmp(zero) > 0 {
-		remainder := new(big.Int)
-		reverse, remainder = reverse.DivMod(reverse, ten, remainder)
-		z01.PrintRune(rune(remainder.Int64() + '0'))
-	}
+func printIntValue(num int) {
+	z01.PrintRune(num)
 }
+
