4fa7d0c37f9af5c7e94f5b9f5077c11623c69875
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-10 16:39:34 +0300

    Tired..

--- printcombn.go
+++ printcombn.go
@@ -4,18 +4,16 @@ import (
 	"github.com/01-edu/z01"
 )
 
-// PrintCombN prints all possible combinations of n different digits in ascending order.
 func PrintCombN(n int) {
 	if n <= 0 || n >= 10 {
-		return // n should be within the range (0, 10)
+		return
 	}
 
 	nums := make([]int, n)
 	printCombination(nums, n, 0, 0)
-	z01.PrintRune('\n') // Print newline after all combinations
+	z01.PrintRune('\n')
 }
 
-// Helper function to recursively generate and print combinations
 func printCombination(nums []int, n, index, start int) {
 	if index == n {
 		printNumber(nums)
@@ -28,7 +26,6 @@ func printCombination(nums []int, n, index, start int) {
 	}
 }
 
-// Helper function to print a combination as a number
 func printNumber(nums []int) {
 	for i, num := range nums {
 		if i > 0 {
@@ -36,6 +33,8 @@ func printNumber(nums []int) {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			} else {
+				z01.PrintRune('0')
+				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 		}
@@ -43,7 +42,6 @@ func printNumber(nums []int) {
 	}
 }
 
-// Helper function to print a single digit
 func printDigit(digit int) {
 	z01.PrintRune(rune(digit) + '0')
 }
