f1448d16ed259859aabc36503e5f0b94b2e60537
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-10 15:19:51 +0300

    Atoi

--- atoi.go
+++ atoi.go
@@ -12,10 +12,15 @@ func Atoi(s string) int {
 	result := 0
 	sign := 1
 
-	for _, c := range s {
-		if c == '-' {
-			sign = -1
-		} else if c >= '0' && c <= '9' {
+	for i, c := range s {
+		if i == 0 && (c == '-' || c == '+') {
+			if c == '-' {
+				sign = -1
+			}
+			continue
+		}
+
+		if c >= '0' && c <= '9' {
 			digit := int(c - '0')
 			result = result*10 + digit
 		} else {
