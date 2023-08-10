dc6eb6cf5519d2a5d45498a8f94a2876c2577f00
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-10 10:27:06 +0300

    Pointone

--- pointone.go
+++ pointone.go
@@ -1,17 +1,7 @@
-package main
+package piscine
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 func PointOne(n *int) {
 	*n = 1
-}
-
-func main() {
-	a := 1
-
-	PointOne(&a)
-
-	var pointer *int = &a
-
-	fmt.Println(*pointer)
 }
