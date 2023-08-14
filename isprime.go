c31928b9fbb8ed1727b6e64370ea2735700eaa0a
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-14 14:15:48 +0300

    Prime numbers

--- isprime.go
+++ isprime.go
@@ -1,10 +1,3 @@
-// Write a function that returns true if the int passed as parameter is a prime number. Otherwise it returns false.
-
-// The function must be optimized in order to avoid time-outs with the tester.
-
-// (We consider that only positive numbers can be prime numbers)
-
-// (We also consider that 1 is not a prime number
 package piscine
 
 func IsPrime(nb int) bool {
@@ -17,7 +10,6 @@ func IsPrime(nb int) bool {
 	if nb%2 == 0 || nb%3 == 0 {
 		return false
 	}
-
 	i := 5
 	for i*i <= nb {
 		if nb%i == 0 || nb%(i+2) == 0 {
@@ -25,6 +17,5 @@ func IsPrime(nb int) bool {
 		}
 		i += 6
 	}
-
 	return true
 }
