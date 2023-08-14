ad18fcddbfdc287f9ad35eaa22251d3f9cf7f7c7
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-14 10:12:53 +0300

    My result

---
+++ recursivefactorial.go
@@ -0,0 +1,16 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	if nb < 0 || nb > 20 {
+		return 0
+	}
+
+	if nb == 1 {
+		return 1
+	}
+
+	if nb > 1 {
+		return nb * RecursiveFactorial(nb-1)
+	}
+	return 0
+}
