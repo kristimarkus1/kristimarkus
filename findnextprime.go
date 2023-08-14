d39cb08e5bfa3107784491b3117aa41af9d83322
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-14 14:08:48 +0300

    next

---
+++ findnextprime.go
@@ -0,0 +1,42 @@
+// Write a function that returns the first prime number that is equal or superior to the int passed as parameter.
+
+// The function must be optimized in order to avoid time-outs with the tester.
+
+// (We consider that only positive numbers can be prime numbers)
+
+package piscine
+
+func FindNextPrime(nb int) int {
+	if nb < 2 {
+		return 2
+	}
+
+	for {
+		if IsPrime(nb) {
+			return nb
+		}
+		nb++
+	}
+}
+
+func IsPrime(nb int) bool {
+	if nb <= 1 {
+		return false
+	}
+	if nb <= 3 {
+		return true
+	}
+	if nb%2 == 0 || nb%3 == 0 {
+		return false
+	}
+
+	i := 5
+	for i*i <= nb {
+		if nb%i == 0 || nb%(i+2) == 0 {
+			return false
+		}
+		i += 6
+	}
+
+	return true
+}
