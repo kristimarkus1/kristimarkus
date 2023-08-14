a752b42b11f023ceeea87070b4e9b450cf8a207e
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-14 13:45:29 +0300

    sqrt.go

--- sqrt.go
+++ sqrt.go
@@ -1,18 +1,17 @@
-// Write a function that returns the square root of the int passed as parameter, 
+// Write a function that returns the square root of the int passed as parameter,
 // if that square root is a whole number. Otherwise it returns 0
 
 package piscine
 
 func Sqrt(nb int) int {
-
 	if nb < 0 {
-		return 0	
+		return 0
 	}
 
 	if nb == 0 || nb == 1 {
 		return nb
 	}
-	
+
 	for i := 1; i*i <= nb; i++ {
 		if i*i == nb {
 			return i
