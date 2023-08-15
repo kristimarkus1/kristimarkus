0fdedbf9644534dbd773a075f29f93900ca8ba06
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-15 09:59:47 +0300

    n rune

--- nrune.go
+++ nrune.go
@@ -4,9 +4,10 @@ package piscine
 
 func NRune(s string, n int) rune {
 	runes := []rune(s)
-
-	if n < 0 || n >= len(runes) {
+	
+	if n <= 0 || n > len(runes) {
 		return 0
 	}
-	return runes[n]
+	return runes[n-1]
 }
+
