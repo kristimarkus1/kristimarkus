00d1c45d44f1c651d66d33d23ce63e15b97f8b43
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-15 16:05:13 +0300

    Capitalize

---
+++ capitalize.go
@@ -0,0 +1,34 @@
+// Write a function that capitalizes the first letter 
+// of each word and lowercases the rest.
+
+package piscine
+
+func Capitalize(s string) string {
+	result := ""
+	capitalizeNext := true
+
+	for _, char := range s {
+		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') {
+			if capitalizeNext {
+				if char >= 'a' && char <= 'z' {
+					result += string(char - 'a' + 'A')
+				} else {
+					result += string(char)
+				}
+				capitalizeNext = false
+			} else {
+				if char >= 'A' && char <= 'Z' {
+					result += string(char - 'A' + 'a')
+				} else {
+					result += string(char)
+				}
+			}
+		} else {
+			result += string(char)
+			capitalizeNext = true
+		}
+	}
+
+	return result
+}
+
