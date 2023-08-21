16122cb8a6c7984d7e0513587a33efcef0e0b6e4
Kristi Markus <kristimarkus@Kristis-MacBook-Air.local>
2023-08-21 14:08:35 +0300

    l

--- ztail/main.go
+++ ztail/main.go
@@ -9,12 +9,13 @@ func main() {
 	args := os.Args[1:]
 
 	if len(args) < 2 || args[0] != "-c" {
-		usage()
+		usageAndExit()
 	}
 
 	count := 0
-	if _, err := fmt.Sscanf(args[1], "%d", &count); err != nil {
-		usage()
+	_, err := fmt.Sscanf(args[1], "%d", &count)
+	if err != nil || count <= 0 {
+		usageAndExit()
 	}
 
 	files := args[2:]
@@ -25,7 +26,12 @@ func main() {
 		if i > 0 {
 			fmt.Println()
 		}
-		if !printTail(filename, count) {
+		if fileExists(filename) {
+			if !printTail(filename, count, len(files) > 1) {
+				success = false
+			}
+		} else {
+			fmt.Printf("open %s: no such file or directory\n", filename)
 			success = false
 		}
 	}
@@ -35,37 +41,60 @@ func main() {
 	}
 }
 
-func usage() {
-	// Print usage message and exit
+func usageAndExit() {
 	fmt.Printf("Usage: %s -c <count> <file1> [<file2> ...]\n", os.Args[0])
-	os.Exit(1)
+	os.Exit(0) // Exit without an error status
 }
 
-func printTail(filename string, count int) bool {
+func printTail(filename string, count int, multipleFiles bool) bool {
+	if multipleFiles {
+		fmt.Printf("==> %s <==\n", filename)
+	}
+
 	file, err := os.Open(filename)
 	if err != nil {
-		errMessage := fmt.Sprintf("open %s: %s\n", filename, err.Error())
-		fmt.Print(errMessage)
+		fmt.Printf("open %s: %s\n", filename, err)
+		if multipleFiles {
+			fmt.Println() // Add an empty line after the error message
+		}
 		return false
 	}
 	defer file.Close()
 
-	stat, _ := file.Stat()
-	if stat.Size() < int64(count) {
-		errMessage := fmt.Sprintf("%s\n", "File size is smaller than count")
-		fmt.Print(errMessage)
+	stat, err := file.Stat()
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		if multipleFiles {
+			fmt.Println() // Add an empty line after the error message
+		}
 		return false
 	}
-	file.Seek(stat.Size()-int64(count), 0) // Seek to the beginning of the count bytes from the end
+
+	if stat.Size() < int64(count) {
+		fmt.Println("File size is smaller than count")
+		if multipleFiles {
+			fmt.Println() // Add an empty line after the error message
+		}
+		return false
+	}
+
+	file.Seek(stat.Size()-int64(count), 0)
 	buffer := make([]byte, count)
 	n, err := file.Read(buffer)
 	if err != nil {
-		errMessage := fmt.Sprintf("%s\n", err.Error())
-		fmt.Print(errMessage)
+		fmt.Printf("%s\n", err)
+		if multipleFiles {
+			fmt.Println() // Add an empty line after the error message
+		}
 		return false
 	}
 
-	fmt.Printf("==> %s <==\n%s\n", filename, buffer[:n])
+	fmt.Printf("%s", buffer[:n])
 
 	return true
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
 }
