d42fc7e510d4ead2bf565499ca114562173dda55
Kristi Markus <kristimarkus@Kristis-Air.lan>
2023-08-21 12:22:32 +0300

    d

--- ztail/main.go
+++ ztail/main.go
@@ -8,54 +8,55 @@ import (
 func main() {
 	args := os.Args[1:]
 
-	if len(args) < 2 || args[0] != "-c" {
+	if len(args) < 3 || args[0] != "-c" {
 		usage()
 	}
 
 	count := 0
-	if _, err := fmt.Sscanf(args[1], "%d", &count); err != nil {
+	_, err := fmt.Sscanf(args[1], "%d", &count)
+	if err != nil {
 		usage()
 	}
 
 	files := args[2:]
 
+	hasError := false
+
 	for _, filename := range files {
-		printTail(filename, count, len(files) > 1)
+		if len(files) > 1 {
+			fmt.Printf("==> %s <==\n", filename)
+		}
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Printf("open %s: %s\n", filename, err.Error())
+			hasError = true
+			continue
+		}
+		defer file.Close()
+
+		stat, _ := file.Stat()
+		file.Seek(stat.Size()-int64(count), 0)
+		buffer := make([]byte, count)
+		n, err := file.Read(buffer)
+		if err != nil {
+			fmt.Printf("%s\n", err.Error())
+			hasError = true
+			continue
+		}
+
+		fmt.Print(string(buffer[:n]))
+
+		if len(files) > 1 {
+			fmt.Print("\n")
+		}
+	}
+
+	if hasError {
+		os.Exit(1)
 	}
 }
 
 func usage() {
-	// Print usage message and exit
 	fmt.Printf("Usage: %s -c <count> <file1> [<file2> ...]\n", os.Args[0])
 	os.Exit(1)
-}
-
-func printTail(filename string, count int, multipleFiles bool) {
-	if multipleFiles {
-		fmt.Printf("==> %s <==\n", filename)
-	}
-
-	file, err := os.Open(filename)
-	if err != nil {
-		errMessage := fmt.Sprintf("open %s: %s\n", filename, err.Error())
-		fmt.Print(errMessage)
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	stat, _ := file.Stat()
-	file.Seek(stat.Size()-int64(count), 0) // Seek to the beginning of the count bytes from the end
-	buffer := make([]byte, count)
-	n, err := file.Read(buffer)
-	if err != nil {
-		errMessage := fmt.Sprintf("%s\n", err.Error())
-		fmt.Print(errMessage)
-		os.Exit(1)
-	}
-
-	fmt.Print(string(buffer[:n]))
-
-	if multipleFiles {
-		fmt.Print("\n")
-	}
 }
