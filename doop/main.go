d33bf54d1c6926d691927e4f0d8c072efc945297
Kristi Markus <kristimarkus@Kristis-Air.lan>
2023-08-22 10:25:16 +0300

    doop

--- doop/main.go
+++ doop/main.go
@@ -5,94 +5,132 @@ import (
 )
 
 func main() {
-	args := os.Args
-
-	if len(args) != 4 {
-		return
-	}
-
-	arg1 := atoi(args[1])
-	arg2 := atoi(args[3])
-	operator := args[2]
-
-	result := 0
-	validOperator := true
-
-	if operator == "+" {
-		result = arg1 + arg2
-	} else if operator == "-" {
-		result = arg1 - arg2
-	} else if operator == "*" {
-		result = arg1 * arg2
-	} else if operator == "/" {
-		if arg2 != 0 {
-			result = arg1 / arg2
-		} else {
-			os.Stdout.Write([]byte("No division by 0\n"))
-			return
+	if len(os.Args) == 4 {
+		args := os.Args[1:]
+		var firstNum int
+		var secondNum int
+		var operator string
+		IsfirstNum := false
+		IsSecondNum := false
+		if string(args[0][0]) == "-" {
+			firstNum, IsfirstNum = findNumber(args[0][1:], args[0])
+		} else if string(args[0][0]) != "-" {
+			firstNum, IsfirstNum = findNumber(args[0], args[0])
 		}
-	} else if operator == "%" {
-		if arg2 != 0 {
-			result = arg1 % arg2
-		} else {
-			os.Stdout.Write([]byte("No modulo by 0\n"))
-			return
+		if string(args[2][0]) != "-" {
+			secondNum, IsSecondNum = findNumber(string(args[2]), string(args[2]))
+		} else if string(args[2][0]) == "-" {
+			secondNum, IsSecondNum = findNumber(args[2][1:], args[2])
 		}
-	} else {
-		validOperator = false
-	}
-
-	if validOperator {
-		printNbr(result)
+		if IsOperator(args[1]) && overFlow(firstNum) && overFlow(secondNum) {
+			operator = args[1]
+			if IsSecondNum && IsfirstNum {
+				switch operator {
+				case "+":
+					os.Stdout.Write(intToByte(firstNum + secondNum))
+				case "-":
+					os.Stdout.Write(intToByte(firstNum - secondNum))
+				case "*":
+					os.Stdout.Write(intToByte(firstNum * secondNum))
+				case "/":
+					if secondNum == 0 {
+						strToByte("No division by 0\n")
+					} else {
+						os.Stdout.Write(intToByte(firstNum / secondNum))
+					}
+				case "%":
+					if secondNum == 0 {
+						strToByte("No modulo by 0\n")
+					} else {
+						os.Stdout.Write(intToByte(firstNum % secondNum))
+					}
+				}
+			}
+		}
 	}
 }
 
-func atoi(s string) int {
-	neg := false
-	num := 0
-
-	for _, c := range s {
-		if c == '-' {
-			neg = true
-		} else if c >= '0' && c <= '9' {
-			num = num*10 + int(c-'0')
-		} else {
-			break
-		}
+func intToByte(num int) []byte {
+	var lists []byte
+	negNum := false
+	if num == 0 {
+		lists = append(lists, 48)
+		lists = append(lists, "\n"[0])
+		return lists
 	}
-
-	if neg {
-		num *= -1
+	if num < 0 {
+		negNum = true
+		num = num * -1
 	}
-
-	return num
+	for 0 < num {
+		lists = append([]byte{byte(num%10 + 48)}, lists...)
+		num = num / 10
+	}
+	if negNum {
+		lists = append([]byte{byte(45)}, lists...)
+	}
+	lists = append(lists, "\n"[0])
+	return lists
 }
 
-func printNbr(n int) {
-	neg := false
-
-	if n < 0 {
-		os.Stdout.Write([]byte("-"))
-		neg = true
-		n *= -1
+func strToByte(s string) {
+	var lists []byte
+	for i := range s {
+		lists = append(lists, s[i])
 	}
+	os.Stdout.Write(lists)
+}
 
-	digit := 1
-	tmp := n
-
-	for tmp >= 10 {
-		tmp /= 10
-		digit *= 10
+func findNumber(s string, s2 string) (int, bool) {
+	if IsNumeric(s) {
+		return Atoi(s2), true
 	}
+	return 0, false
+}
 
-	for digit > 0 {
-		digitChar := byte(n/digit + '0')
-		os.Stdout.Write([]byte{digitChar})
-		n %= digit
-		digit /= 10
+func overFlow(num int) bool {
+	if num < 9223372036854775807 && -9223372036854775807 < num {
+		return true
 	}
+	return false
+}
 
-	if neg {
-		n *= -1
+func IsOperator(s string) bool {
+	if s == "+" || s == "-" || s == "/" || s == "%" || s == "*" {
+		return true
 	}
+	return false
+}
+
+func IsNumeric(s string) bool {
+	for _, symbol := range s {
+		if symbol >= 48 && symbol <= 57 {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+func Atoi(s string) int {
+	newInt := 0
+	var number int
+	plussorminus := 1
+	for posnum, symbol := range s {
+		if posnum == 0 && symbol == 43 {
+			continue
+		} else if posnum == 0 && symbol == 45 {
+			plussorminus = -1
+		} else if symbol >= 48 && symbol <= 57 {
+			for i := '0'; symbol > i; i++ {
+				number++
+			}
+			newInt = (newInt * 10) + number
+			number = 0
+		} else {
+			return 0
+		}
+	}
+	return newInt * plussorminus
 }
