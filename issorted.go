7b4f44eee9996c81203a013aaed38c1012095b50
Kristi Markus <kristimarkus@Kristis-Air.lan>
2023-08-22 09:17:58 +0300

    sorted

--- issorted.go
+++ issorted.go
@@ -2,10 +2,18 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	n := len(a)
+	ascending := true
+	descending := true
+
 	for i := 1; i < n; i++ {
-		if f(a[i-1], a[i]) > 0 {
-			return false
+		result := f(a[i-1], a[i])
+		if ascending && result > 0 {
+			ascending = false
+		}
+		if descending && result < 0 {
+			descending = false
 		}
 	}
-	return true
+
+	return ascending || descending
 }
