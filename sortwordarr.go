f21c0cf49ed7d0927312c2534d0fdaf1e68685ef
Kristi Markus <kristimarkus@Kristis-Air.lan>
2023-08-22 10:30:25 +0300

    sortword

---
+++ sortwordarr.go
@@ -0,0 +1,13 @@
+package piscine
+
+func SortWordArr(a []string) {
+	// Bubble sort implementation
+	n := len(a)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
+}
