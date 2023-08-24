a47d1589c2799618b4cff3e8a2def529b4372e82
kmarkus <kristimarkus@gmail.com>
2023-08-24 12:29:11 +0300

    dada

---
+++ listremoveif.go
@@ -0,0 +1,23 @@
+package piscine
+
+func ListRemoveIf(l *List, data_ref interface{}) {
+	if l == nil {
+		return
+	}
+	current := l.Head
+	var previous *NodeL
+
+	for current != nil {
+		if current.Data == data_ref {
+			if previous == nil {
+				l.Head = current.Next
+			} else {
+				previous.Next = current.Next
+				current = previous
+			}
+		} else {
+			previous = current
+		}
+		current = current.Next
+	}
+}
